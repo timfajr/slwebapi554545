dc8d84751cf16546962cf166a8bde1a276638b21
Tim Fajr <timfajr@gmail.com>
2022-11-19 20:51:36 +0700

    cors

--- router/user.js
+++ router/user.js
@@ -11,26 +11,26 @@ var utc_timestamp = Date.UTC(now.getUTCFullYear(),now.getUTCMonth(), now.getUTCD
 router.use(express.json());
 
 router.post('/register', async (request, response) => {
-    const data = await Device.find({deviceid : {$regex: request.body.deviceid}});
-    if(data[0].deviceid == request.body.deviceid){
-        response.status(400).json({message: "user already registered !"})
-    }
-    else {
     const registeruser = new Device({
         deviceid: request.body.deviceid,
         ownerid: request.body.ownerid,
         activeregion: [{ regionurl : request.body.regionurl, login_date: utc_timestamp }],
         created_at: utc_timestamp
     })
-    
     try {
+        const data = await Device.find({deviceid : {$regex: request.body.deviceid}});
+        if(data[0] && data[0].deviceid == request.body.deviceid){
+            response.status(400).json({message: "user already registered !"})
+        }
+        else {
         const registerToSave = await registeruser.save();
         response.status(200).json({message: "success", access_token: "token333"});
+        }
     }
     catch (error) {
         response.status(400).json({message: error.message})
     }
-  }
+
 })
 
 router.get('/token', async (req, res) => {
